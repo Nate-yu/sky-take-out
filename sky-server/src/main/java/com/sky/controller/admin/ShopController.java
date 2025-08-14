a500fe984a5fb7e081631f5f11b4e7848a79ef18
Nate-yu <2242936124@qq.com>
2025-08-14 20:47:59 +0800

    店铺营业状态设置代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,55 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author yubin
+ * @date 2025年08月14日 19:48
+ */
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    // 自动注入redis模板对象
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 修改店铺状态
+     * @author yubin
+     * @date 2025/8/14 19:54
+     * @param status
+     * @return com.sky.result.Result
+     */
+    @PutMapping("/{status}")
+    @ApiOperation(value = "修改店铺状态")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺营业状态为：{}",status == 1 ? "营业" : "打烊");
+        redisTemplate.opsForValue().set(KEY,status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺状态
+     * @author yubin
+     * @date 2025/8/14 19:59
+     * @return com.sky.result.Result<java.lang.Integer>
+     */
+    @GetMapping("/status")
+    @ApiOperation(value = "获取店铺状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺营业状态为：{}",status == 1 ? "营业" : "打烊");
+        return Result.success(status);
+    }
+}
